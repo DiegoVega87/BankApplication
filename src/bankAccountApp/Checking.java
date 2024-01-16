9ce07e587bb0441eeb98c33b3d38b82c19648d4a
DiegoVega87 <diegovega@me.com>
2024-01-16 10:45:29 +0100

    Added contstrutors and overrode methods in Checkin and Savings classes

--- src/bankAccountApp/Checking.java
+++ src/bankAccountApp/Checking.java
@@ -2,4 +2,23 @@ package bankAccountApp;
 
 public class Checking extends Account{
 
+
+    public Checking(String firstName, String lastName, String SSN, double initialDeposit, String type) {
+        super(firstName, lastName, SSN, initialDeposit, type);
+    }
+
+    @Override
+    public boolean desposit(double amount) {
+        return false;
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        return false;
+    }
+
+    @Override
+    public boolean transfer(String accountNumber, double amount) {
+        return false;
+    }
 }
--- src/bankAccountApp/Savings.java
+++ src/bankAccountApp/Savings.java
@@ -1,4 +1,22 @@
 package bankAccountApp;
 
 public class Savings extends Account{
+    public Savings(String firstName, String lastName, String SSN, double initialDeposit, String type) {
+        super(firstName, lastName, SSN, initialDeposit, type);
+    }
+
+    @Override
+    public boolean desposit(double amount) {
+        return false;
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        return false;
+    }
+
+    @Override
+    public boolean transfer(String accountNumber, double amount) {
+        return false;
+    }
 }
