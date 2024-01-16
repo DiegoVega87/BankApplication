f03d2da58ccf6c8d65be61fa1c07e1d06d4c20ac
DiegoVega87 <diegovega@me.com>
2024-01-16 10:43:23 +0100

    Add constructor to Account class

--- src/bankAccountApp/Account.java
+++ src/bankAccountApp/Account.java
@@ -9,6 +9,13 @@ public abstract class Account implements IBaseRate {
     private String accountNumber;
     private String type;
 
+    public Account(String firstName, String lastName, String SSN, double initialDeposit, String type) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.SSN = SSN;
+        this.initialDeposit = initialDeposit;
+        this.type = type;
+    }
 
     public abstract boolean desposit(double amount);
     public abstract boolean withdraw(double amount);
