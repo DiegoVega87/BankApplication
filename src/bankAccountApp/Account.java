9dd791cd5f2786676960f4d08039667ba09dd66a
DiegoVega87 <diegovega@me.com>
2024-01-16 12:28:53 +0100

    Add setAccountNumber method to Account class

--- src/bankAccountApp/Account.java
+++ src/bankAccountApp/Account.java
@@ -1,11 +1,14 @@
 package bankAccountApp;
 
+import java.util.Random;
+
 public abstract class Account implements IBaseRate {
 
     private String name;
     private String SSN;
     private double balance;
     private String accountNumber;
+    private static int digits = 10000;
     double rate;
 
     public Account(String name, String SSN, double initialDeposit) {
@@ -14,8 +17,21 @@ public abstract class Account implements IBaseRate {
         balance = initialDeposit;
 
         System.out.println("Name: " + name + "\sSSN: " + SSN + "\sBalance: " + balance);
+
+        //set account number
+        setAccountNumber(this.getClass().getSimpleName(), SSN);
+        System.out.println(accountNumber);
     }
 
+    private void setAccountNumber(String accountType, String SSN){
+
+        String twoSSNDigits = SSN.substring(SSN.length()-2);
+        Random random = new Random();
+        int rDigits = random.nextInt(100, 900);
+        int firstDigit = accountType.equalsIgnoreCase("savings") ? 1 : 2;
+
+        accountNumber = firstDigit + twoSSNDigits + digits + rDigits;
+    }
     public abstract boolean desposit(double amount);
     public abstract boolean withdraw(double amount);
 
--- src/bankAccountApp/BankAccountApp.java
+++ src/bankAccountApp/BankAccountApp.java
@@ -1,9 +1,17 @@
 package bankAccountApp;
 
+import java.util.Random;
+
 public class BankAccountApp {
 
     public static void main(String[] args) {
         Checking chkcc1 = new Checking("Tom Wilson", "937184575", 1500);
         Savings savacc1 = new Savings("Rich Lowe", "456217852", 2500);
+
+        Random random = new Random();
+        int number = random.nextInt(100, 999);
+        System.out.println(number);
+
+
     }
 }
